586d51a894e84c70b91ca760d37812fd2941fe27
AleksanderBarylak <76066072+AleksanderBarylak@users.noreply.github.com>
2020-12-20 17:48:29 +0100

    create msg model

--- foksite/main/models.py
+++ foksite/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Msg(models.Model):
+    msg_author_username = models.CharField(max_length=35)
+    msg_content = models.TextField()
+    msg_send_time = models.DateTimeField("sended")
+
+    def __str__(self):
+        n1 = '\n'
+        return f"{msg_author_username}{n1}{msg_content}{n1}{msg_send_time}"
