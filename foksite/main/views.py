1ef1fcbc0f43b32e202e26c79dbffc980eabf81c
AleksanderBarylak <76066072+AleksanderBarylak@users.noreply.github.com>
2020-12-20 17:01:41 +0100

    create main app

--- foksite/main/views.py
+++ foksite/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse("123")        #return html here
 
 # Create your views here.
